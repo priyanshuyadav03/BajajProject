176ebec1aa47f1816d00020a61b8cb6fae87ac81
priyanshu yadav <ypriyanshu5714@gmail.com>
2026-05-26 15:53:59 +0530

    Update test to use @MockitoBean for compatibility with Spring Boot 3.4.1

--- src/test/java/com/bajaj/bfhl/controller/BfhlControllerTest.java
+++ src/test/java/com/bajaj/bfhl/controller/BfhlControllerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.bean.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,7 +28,7 @@ class BfhlControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private BfhlService bfhlService;
 
     @Autowired
